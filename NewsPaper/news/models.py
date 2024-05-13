3ef8cadf6059a8b0f46a3cf1be902b053b33ca87
Kormin <krazkms@mail.ru>
2024-05-13 21:52:51 +0300

    second

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -9,7 +9,7 @@ from django.db.models.functions import Coalesce
 
 class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    rating = models.IntegerField(default=0)
+    rating = models.SmallIntegerField(default=0)
 
     def __str__(self):
         return self.user
@@ -27,16 +27,19 @@ class Author(models.Model):
 class Category(models.Model):
     category = models.CharField(max_length=100, default='article', unique=True)
 
+    def __str__(self):
+        return f'{self.category}'
+
 
 class Post(models.Model):
-    article = 'AR'
-    news = 'NE'
+    # article = 'AR'
+    # news = 'NE'
 
     categor = [
-        ('article', 'Статья'),
-        ('news', 'Новость'),
+        ('AR', 'Статья'),
+        ('NE', 'Новость'),
     ]
-    type = models.CharField(max_length=10, choices=categor, default=article)
+    type = models.CharField(max_length=8, choices=categor, default='AR')
     user = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='post')
     category = models.ManyToManyField(Category, through='PostCategory')
     date = models.DateTimeField(auto_now_add=True)
@@ -45,7 +48,7 @@ class Post(models.Model):
     rating = models.SmallIntegerField(default=0)
 
     def preview(self):
-        return self.text[:124] + '...'
+        return f'{self.text[0:123]} ...'
 
     def like(self):
         self.rating += 1
@@ -55,10 +58,16 @@ class Post(models.Model):
         self.rating -= 1
         self.save()
 
+    def __str__(self):
+        return f'{self.title}\n{self.text}'
+
 
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.category}'
 
 
 class Comment(models.Model):
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Author, Category, Post, PostCategory, Comment
+from pprint import pprint
+
 
 # Create your views here.
+
+
+class NewsList(ListView):
+    # Указываем модель, объекты которой мы будем выводить
+    model = Post
+    # Поле, которое будет использоваться для сортировки объектов
+    ordering = '-date'
+    # Указываем имя шаблона, в котором будут все инструкции о том,
+    # как именно пользователю должны быть показаны наши объекты
+    template_name = 'news.html'
+    # Это имя списка, в котором будут лежать все объекты.
+    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
+    context_object_name = 'news'
+
+
+class NewDetail(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельному товару
+    model = Post
+    # Используем другой шаблон — product.html
+    template_name = 'new.html'
+    # Название объекта, в котором будет выбранный пользователем продукт
+    context_object_name = 'new'
+
+    def get_context_data(self, **kwargs):
+        # С помощью super() мы обращаемся к родительским классам
+        # и вызываем у них метод get_context_data с теми же аргументами,
+        # что и были переданы нам.
+        # В ответе мы должны получить словарь.
+        context = super().get_context_data(**kwargs)
+        # К словарю добавим текущую дату в ключ 'time_now'.
+        context['tag'] = Post.category.value('category')
+        # Добавим ещё одну пустую переменную,
+        # чтобы на её примере рассмотреть работу ещё одного фильтра.
+        # context['next_sale'] = None
+        pprint(context)
+        return context
